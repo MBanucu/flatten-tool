0777ca4bedb82b092f5cd470804b2c52dcd5b441
MBanucu <michael.banucu@googlemail.com>
2026-02-03 18:22:02 +0100

    refactor: simplify flake to use wrapper with makeWrapper
    Reverts to a simple wrapper approach using makeWrapper for
    runtime Bun execution, ensuring the package is installable.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    bun2nix.url = "github:nix-community/bun2nix";
   };
 
   outputs =
@@ -12,47 +11,11 @@
       self,
       nixpkgs,
       flake-utils,
-      bun2nix,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        bunPkgs = import ./bun-packages.nix {
-          inherit (pkgs)
-            copyPathToStore
-            fetchFromGitHub
-            fetchgit
-            fetchurl
-            ;
-        };
-
-        # Function to get package directory name from full name
-        packageName =
-          name:
-          let
-            parts = builtins.filter builtins.isString (builtins.split "@" name);
-          in
-          if builtins.length parts >= 3 && builtins.head parts == "" then
-            "@" + builtins.elemAt parts 1 + "/" + builtins.elemAt parts 2
-          else
-            builtins.head parts;
-
-        # Create node_modules as a separate derivation by unpacking packages
-        nodeModules = pkgs.runCommand "flatten-tool-node-modules" { } ''
-          mkdir -p $out/node_modules
-          ${builtins.concatStringsSep "\n" (
-            builtins.attrValues (
-              builtins.mapAttrs (
-                name: path:
-                let
-                  pkgDir = packageName name;
-                in
-                "mkdir -p $out/node_modules/${pkgDir}; tar -xzf ${path} -C $out/node_modules/${pkgDir} --strip-components=1"
-              ) bunPkgs
-            )
-          )}
-        '';
       in
       {
         packages.flatten-tool = pkgs.stdenv.mkDerivation {
@@ -60,15 +23,12 @@
           version = "1.0.0";
           src = ./.;
           nativeBuildInputs = [ pkgs.bun ];
-          buildPhase = ''
-            export HOME=$TMPDIR
-            cp -r ${nodeModules}/node_modules node_modules
-            bun install --frozen-lockfile
-            bun build ./index.ts --compile --outfile flatten-tool
-          '';
+          buildInputs = [ pkgs.makeWrapper ];
           installPhase = ''
             mkdir -p $out/bin
-            cp flatten-tool $out/bin/
+            cp index.ts $out/
+            makeWrapper ${pkgs.bun}/bin/bun $out/bin/flatten-tool \
+              --add-flags $out/index.ts
           '';
         };
         packages.default = self.packages.${system}.flatten-tool;
